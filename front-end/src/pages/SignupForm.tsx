9f09d1627abaf415d7c56c592cba46645be2826c
ShadowCode007 <plamen24yavor@gmail.com>
2023-12-09 22:43:25 +0200

    Style SignUp page.Close #4

--- front-end/src/pages/SignupForm.tsx
+++ front-end/src/pages/SignupForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
-import Nav from '../components/Nav';
+import logo from "../resources/Change pres logo.png"
 
 const SignupForm = () => {
     const formik = useFormik({
@@ -17,56 +17,70 @@ const SignupForm = () => {
     });
 
     return (
-        <div>
-            <Nav/>
-
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-                <br />
-                <label htmlFor="email">Email Address</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-                <br />
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-                <br />
-                <label htmlFor="firstname">First Name</label>
-                <input
-                    id="firstname"
-                    name="firstname"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-                <br />
-                <label htmlFor="lastname">Last Name</label>
-                <input
-                    id="lastname"
-                    name="lastname"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-
-            </form>
+        <div className="bg-darkBlue flex items-center justify-center min-h-screen">
+            <div className="flex flex-col items-center w-[60%] mx-auto">
+                <img src={logo} alt="logo" className="mb-4"/>
+                <form className="border px-20 py-10 my rounded-[3rem] bg-white" onSubmit={formik.handleSubmit}>
+                    <label htmlFor="username"></label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.username}
+                        className="border p-2 mb-4"
+                        placeholder="Username"
+                    />
+                    <br />
+                    <label htmlFor="email"></label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        className="border p-2 mb-4"
+                        placeholder="Email"
+                    />
+                    <br />
+                    <label htmlFor="password"></label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        className="border p-2 mb-4"
+                        placeholder="Password"
+                    />
+                    <br />
+                    <label htmlFor="firstName"></label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                        className="border p-2 mb-4"
+                        placeholder="First Name"
+                    />
+                    <br />
+                    <label htmlFor="lastName"></label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.lastName}
+                        className="border p-2 mb-4"
+                        placeholder="Last Name"
+                    />
+                    <div className="flex justify-between mt-4">
+                        <button type="submit" className="mt-4">Sign Up</button>
+                        <button className="mt-4">Log In </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
