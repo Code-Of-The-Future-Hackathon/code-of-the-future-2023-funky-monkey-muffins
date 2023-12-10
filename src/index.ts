9c40cf70ff8d7e9fecc176691eb186921420d13b
AEManov20 <AEManov20@codingburgas.bg>
2023-12-10 05:25:46 +0200

    Implement User service

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import passport from 'passport';
 import './utils/auth';
+import session from 'express-session';
+
 import AuthRouter from './routers/auth';
-import expressSession from 'express-session';
-import router from './routers/api/psychologist';
+import PsychologistRouter from './routers/api/psychologist';
+import UserRouter from './routers/api/user';
 
 const app = express()
 
-app.use(expressSession({
+app.use(session({
   secret: "string",
   resave: true,
   saveUninitialized: true
@@ -15,13 +17,15 @@ app.use(expressSession({
 
 app.use(express.json())
 app.use(passport.initialize())
-app.use(passport.session());
+app.use(passport.session())
 
-app.use('/api', router);
+app.use('/api/psychologist', PsychologistRouter);
+app.use('/api/user', UserRouter);
 
 app.use('/auth', AuthRouter);
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('*wind noises*')
 })
 
 app.listen(3000, () => console.log('server is running'));
--- src/routers/api/user.ts
+++ src/routers/api/user.ts
@@ -1,19 +1,71 @@
 import express from 'express';
 import passport from 'passport';
-import PrismaClient from '../../db';
+import prisma from '../../db';
+import type { User } from '@prisma/client';
+import { UpdateUserFormSchema } from '../../model';
 
 const router = express.Router();
 
-// TODO: return json according to the UserInfo view
 router.get('/@me', passport.authenticate('session'), (req, res) => {
-    console.log(req.user);
-    res.json(req.user);
+    if (req.user === undefined) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+    prisma.user.findUnique({
+        where: {
+            id: (req.user as User).id
+        },
+        select: {
+            createdAt: true,
+            id: true,
+            firstName: true,
+            lastName: true,
+            phoneNumber: true,
+            email: true,
+            username: true,
+        }
+    }).then(user => {
+        if (!user) {
+            req.logout(function(err) {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ message: 'Internal server error' });
+                }
+
+                return res.status(401).json({ message: 'Unauthorized' });
+            });
+
+            return;
+        }
+
+        res.json({
+            createdAt: user.createdAt,
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            phoneNumber: user.phoneNumber,
+            email: user.email,
+            username: user.username,
+        });
+    }).catch(err => {
+        res.status(500);
+        res.json(err);
+    })
 })
 
 router.get('/:id', (req, res) => {
-    PrismaClient.userInfo.findUnique({
+    prisma.user.findUnique({
         where: {
             id: req.params.id
+        },
+        select: {
+            createdAt: true,
+            id: true,
+            firstName: true,
+            lastName: true,
+            phoneNumber: true,
+            email: true,
+            username: true,
         }
     }).then(user => {
         if (!user) {
@@ -21,14 +73,55 @@ router.get('/:id', (req, res) => {
         }
 
         res.json({
+            createdAt: user.createdAt,
             id: user.id,
-            username: user.username,
             firstName: user.firstName,
             lastName: user.lastName,
+            phoneNumber: user.phoneNumber,
             email: user.email,
+            username: user.username,
         });
     }).catch(err => {
         res.status(500);
         res.json(err);
     })
 });
+
+router.put('/@me', passport.authenticate('session'), (req, res) => {
+    if (!req.user) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    let user = req.user as User;
+    let result = UpdateUserFormSchema.safeParse(req.body);
+
+    if (result.success === false) {
+        res.status(400);
+        res.send({ message: 'Invalid data', data: result.error });
+        return;
+    }
+
+    let form = result.data;
+
+    prisma.user.update({
+        where: {
+            id: user.id
+        },
+        data: form
+    }).then(user => {
+        res.json({
+            createdAt: user.createdAt,
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            phoneNumber: user.phoneNumber,
+            email: user.email,
+            username: user.username,
+        });
+    }).catch(err => {
+        res.status(500);
+        res.json(err);
+    })
+});
+
+export default router;
